64c5c82711a375bd31ebe64a3db422d0b6dd9fa7
Celil Eren Kalkan <60541083+CelilErenKalkan@users.noreply.github.com>
2021-12-19 17:31:56 +0300

    Creating Random Mesh Object
    Creating random mesh object via editor and saving it as a prefab.

---
+++ Prototype_Pattern/Assets/Editor/AsteroidSpawnerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(AsteroidSpawner))]
+public class AsteroidSpawnerEditor : Editor
+{
+    private string path;
+    private string assetpath;
+    private string filename;
+
+    void OnEnable()
+    {
+        path = Application.dataPath + "/Asteroid";
+        assetpath = "Assets/Asteroid/";
+        filename = "Asteroid_" + System.DateTime.Now.Ticks.ToString();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AsteroidSpawner astSpawner = (AsteroidSpawner) target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Create Asteroid"))
+        {
+            astSpawner.CreateAsteroid();
+        }
+
+        if (GUILayout.Button("Save Asteroid"))
+        {
+            System.IO.Directory.CreateDirectory(path);
+            Mesh mesh = astSpawner.asteroid.GetComponent<MeshFilter>().sharedMesh;
+            AssetDatabase.CreateAsset(mesh, assetpath + mesh.name + ".asset");
+            AssetDatabase.SaveAssets();
+
+            PrefabUtility.SaveAsPrefabAsset(astSpawner.asteroid, assetpath + filename + ".prefab");
+        }
+    }
+}
